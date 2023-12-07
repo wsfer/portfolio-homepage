2555b025bd28205f404bdb7d5f426afce24e0b0c
wsfer <93054415+wsfer@users.noreply.github.com>
2023-12-07 16:59:27 -0300

    feat: create projects page section component

---
+++ src/components/projects.js
@@ -0,0 +1,30 @@
+import projectCard from './projectCard';
+import placeholderImg from '../assets/images/placeholder-1.jpg';
+
+const temporaryProduct = {
+  name: 'Lorem ipsum',
+  description:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus rerum quis, magni libero, doloribus cupiditate, amet rem quos earum modi natus ut? Voluptate eos fugiat provident praesentium eius delectus saepe.',
+  image: placeholderImg,
+  source: '#',
+  live: '#',
+};
+
+const projectsTemplate = `
+    <section class="projects">
+      <div class="projects__inner">
+        <h2 class="projects__title">My work</h2>
+        <ul class="projects__list">
+          <li class="projects__item">${projectCard(temporaryProduct)}</li>
+          <li class="projects__item">${projectCard(temporaryProduct)}</li>
+          <li class="projects__item">${projectCard(temporaryProduct)}</li>
+          <li class="projects__item">${projectCard(temporaryProduct)}</li>
+          <li class="projects__item">${projectCard(temporaryProduct)}</li>
+        </ul>
+      </div>
+    </section>
+`;
+
+const projects = new Range().createContextualFragment(projectsTemplate);
+
+export default projects;
