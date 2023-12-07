c8642706e3d169725328d2c1380af3e2a9df575c
wsfer <93054415+wsfer@users.noreply.github.com>
2023-12-07 16:55:56 -0300

    feat: create about page section component

---
+++ src/components/about.js
@@ -0,0 +1,24 @@
+import socialMediaLinks from './socialMediaLinks';
+import mainImage from '../assets/images/placeholder-1.jpg';
+
+const aboutTemplate = `
+  <section class="about">
+    <div class="about__inner">
+      <picture class="about__picture picture">
+        <img class="picture__image" src=${mainImage} alt="Lorem ipsum" />
+        <figcaption class="picture__caption">Lorem ipsum</figcaption>
+      </picture>
+      <div class="about__content">
+        <h2 class="about__title">About me</h2>
+        <p class="about__description">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus id atque dolore iure hic voluptas adipisci itaque. Laborum, tempore sit totam ab ducimus aperiam illo! Magni numquam sint eaque accusantium.
+        </p>
+        ${socialMediaLinks('about')}
+      </div>
+    </div>
+  </section>
+`;
+
+const about = new Range().createContextualFragment(aboutTemplate);
+
+export default about;
