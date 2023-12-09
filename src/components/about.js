1b185ced69105a02a5b2e53a74141f896e88a10d
wsfer <93054415+wsfer@users.noreply.github.com>
2023-12-09 15:02:39 -0300

    feat: small changes on about section placeholder content

--- src/components/about.js
+++ src/components/about.js
@@ -6,11 +6,13 @@ const aboutTemplate = `
     <div class="about__inner">
       <picture class="about__picture picture">
         <img class="picture__image" src=${mainImage} alt="Lorem ipsum" />
-        <figcaption class="picture__caption">Lorem ipsum</figcaption>
+        <figcaption class="picture__caption">Lorem ipsum dolor sit</figcaption>
       </picture>
       <div class="about__content">
         <h2 class="about__title">About me</h2>
         <p class="about__description">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus id atque dolore iure hic voluptas adipisci itaque. Laborum, tempore sit totam ab ducimus aperiam illo! Magni numquam sint eaque accusantium.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus id atque dolore iure hic voluptas adipisci itaque. Laborum, tempore sit totam ab ducimus aperiam illo! Magni numquam sint eaque accusantium.
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus id atque dolore iure hic voluptas adipisci itaque. Laborum, tempore sit totam ab ducimus aperiam illo! Magni numquam sint eaque accusantium.
         </p>
         ${socialMediaLinks('about')}
