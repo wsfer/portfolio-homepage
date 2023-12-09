dc6b03b8c3239e986a9a0e323c4da4dee55dbc23
wsfer <93054415+wsfer@users.noreply.github.com>
2023-12-09 16:55:52 -0300

    feat: update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
+import about from './components/about';
+import projects from './components/projects';
+import footer from './components/footer';
 import './styles/main.scss';
+
+document.body.appendChild(about);
+document.body.appendChild(projects);
+document.body.appendChild(footer);
